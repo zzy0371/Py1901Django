b1991d9a8071fce018b5a556a2b2fa9b04079bb2
zzy <496575233@qq.com>
2019-04-17 16:31:49 +0800

    模板填充

--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -7,25 +7,27 @@ from django.template import loader
 # 定义视图函数
 def index(request):
     # return HttpResponse("首页")
+
     # 加载模板
-    indextem = loader.get_template('booktest/index.html')
-    cont = {"username":"zzy"}
-    # 使用变量参数渲染模板
-    result = indextem.render(cont)
-    # 返回模板
-    return HttpResponse(result)
+    # indextem = loader.get_template('booktest/index.html')
+    # cont = {"username":"zzy"}
+    # # 使用变量参数渲染模板
+    # result = indextem.render(cont)
+    # # 返回模板
+    # return HttpResponse(result)
+
+    return render(request,'booktest/index.html',{"username":'zzy'})
 
 
 def list(request):
-    return HttpResponse("列表页")
+    # return HttpResponse("列表页")
+    bl = BookInfo.objects.all()
+    return render(request,'booktest/list.html',{"booklist":bl})
 
 
 def detail(request,id):
-    try:
-        book = BookInfo.objects.get(pk=int(id))
-        return HttpResponse(book)
-    except:
-        return HttpResponse("请输入正确id")
+    book = BookInfo.objects.get(pk=id)
+    return render(request,'booktest/detail.html',{"book":book})
 """
 视图函数
 将函数和路绑定
