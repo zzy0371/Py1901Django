dd93e6b1b261d5a63daf6e7cd3f7380a21769bcb
zzy <496575233@qq.com>
2019-04-17 14:33:44 +0800

    视图传参

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    # url('index/', views.index),
-    # url('index/$', views.index),
-    # url(r'index/$', views.index),
-    # url('list/', views.list),
-    url('index/$', views.index),
-    url('list/$',views.list),
+    url(r'^$', views.index),
+    url(r'^list/$',views.list),
+    url(r'^detail/(\d+)/$',views.detail)
 ]
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BookInfo
 # Create your views here.
 
 # 定义视图函数
@@ -11,6 +12,13 @@ def index(request):
 def list(request):
     return HttpResponse("列表页")
 
+
+def detail(request,id):
+    try:
+        book = BookInfo.objects.get(pk=int(id))
+        return HttpResponse(book)
+    except:
+        return HttpResponse("请输入正确id")
 """
 视图函数
 将函数和路绑定
