4f8528cfba9f152c1cbee226dcedc1006b612576
zzy <496575233@qq.com>
2019-04-24 15:15:29 +0800

    博客添加模板

--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -5,24 +5,24 @@ from .models import Post,Tag,Category
 
 def index(request):
     postlist = Post.objects.all()
-    print(postlist)
-    return HttpResponse("首页")
+    # print(postlist)
+    return render(request,'blog/index.html')
 
 def detail(request,id):
     post = get_object_or_404(Post,pk = id)
     print(post)
-    return HttpResponse("详情页")
+    return render(request,'blog/single.html')
 
 def archives(request,y,m):
     postlist = get_list_or_404(Post, create_time__year = y, create_time__month = m)
-    return HttpResponse("首页")
+    return render(request, 'blog/index.html')
 
 def category(request,id):
     postlist = get_object_or_404(Category,pk=id).post_set.all()
     print(postlist)
-    return HttpResponse("首页")
+    return render(request, 'blog/index.html')
 
 def tag(request,id):
     postlist = get_object_or_404(Tag,pk=id).post_set.all()
     print(postlist)
-    return HttpResponse("首页")
+    return render(request, 'blog/index.html')
